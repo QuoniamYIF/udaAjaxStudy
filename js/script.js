8e978e4bd5f81e11f07205e8291b4d83b677647e
QuoniamYIF <quoniamy@gmail.com>
2016-09-21 00:52:20 +0800

    加入维基百科

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 
 function loadData() {
-
     var $body = $('body');
     var $wikiElem = $('#wikipedia-links');
     var $nytHeaderElem = $('#nytimes-header');
@@ -12,9 +11,44 @@ function loadData() {
     $nytElem.text("");
 
     // load streetview
-
     // YOUR CODE GOES HERE!
+    var streetStr = $("#street").val();
+    var cityStr = $("#city").val();
+    var address = streetStr + ', ' + cityStr;
+    $greeting.text('So, you want to live at ' + address + '?');
 
+    var streetviewUrl = 'https://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
+    $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
+
+    var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=c618bff58a144d7487ecd7ee3f36b95b';
+    var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + cityStr +'&format=json&callback=wikiCallback';
+    $.getJSON(nytimesUrl, function(data){
+        $nytHeaderElem.text('New York Times Articles About ' + cityStr);
+        articles = data.response.docs;
+        for(var i = 0; i < articles.length; i ++) {
+            var article = articles[i];
+            $nytElem.append('<li class="article">'+
+                '<a href="'+article.web_url+'">'+article.headline.main+
+                    '</a>'+
+                '<p>'+article.snippet+'</p>'+
+            '</li>');
+        }
+    }).fail(function(e){
+        $nytHeaderElem.text('New York Times Articles doesn\'t be loaded');
+    });
+
+    $.ajax({
+        url: wikiUrl,
+        dataType: "jsonp",
+        success: function(response) {
+            var articleList = response[1];
+            for(var i = 0;i < articleList.length; i++) {
+                articleStr = articleList[i];
+                var url = 'http://en.wikipedia.org/wiki/' + articleStr;
+                $wikiElem.append('<li><a href="' +url + '">' + articleStr + '</a></li>');
+            };
+        }
+    });
     return false;
 };
 
